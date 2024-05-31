87f54c159b0632c8d69509908ae2ef16841fd3ed
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2024-05-31 20:33:08 +0200

    feat: completed the Engine interface class

--- models.py
+++ models.py
@@ -19,14 +19,31 @@ class ModelManager:
 
     def get_engine(self, engine, path):
         if engine == "exllamav2":
-            return Testengine(path)
+            return Engine(path)
         else:
             raise ValueError(f"Unknown engine: {engine}")
 
 
-class Testengine:
+class Engine:
     def __init__(self, path):
         self.path = path
+        self.parameters = None
+
+    def set_parameters(self, parameters):
+        self.parameters = parameters
+
+    def reload_model(self):
+        self.unload_model()
+        self.load_model()
 
     def load_model(self):
-        return True
+        raise NotImplementedError
+
+    def unload_model(self):
+        raise NotImplementedError
+
+    def complete(self, prompt):
+        raise NotImplementedError
+
+    def complete_streaming(self, prompt, streaming_callback):
+        raise NotImplementedError
