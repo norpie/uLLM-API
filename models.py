155d9af99b4025c783eee53b81fc016da476a29b
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2024-05-31 18:33:38 +0200

    feat: added boilerplate for model engines

--- models.py
+++ models.py
@@ -1,7 +1,32 @@
 class ModelManager:
     def __init__(self, data_dir):
         self.data_dir = data_dir
+        self.engine = None
+
+    def load_model(self, engine, model_name):
+        if model_name not in self.list_models():
+            raise ValueError(f"Model {model_name} not found")
+
+        engine = self.get_engine(engine, self.data_dir.get_model_path() / model_name)
+        engine.load_model()
 
     def list_models(self):
         model_path = self.data_dir.get_model_path()
         return [f.name for f in model_path.iterdir() if f.is_dir()]
+
+    def current_engine(self):
+        return self.engine
+
+    def get_engine(self, engine, path):
+        if engine == "exllamav2":
+            return Testengine(path)
+        else:
+            raise ValueError(f"Unknown engine: {engine}")
+
+
+class Testengine:
+    def __init__(self, path):
+        self.path = path
+
+    def load_model(self):
+        return True
--- web.py
+++ web.py
@@ -1,15 +1,24 @@
 from fastapi import FastAPI
 import uvicorn
 
+
 def run(host, port, model_manager):
     app = FastAPI()
 
     @app.get("/ping")
     def ping():
-        return "pong"
+        return {"ping": "pong"}
 
     @app.get("/models")
     def list_models():
         return model_manager.list_models()
+
+    @app.post("/models/{engine}/{model_name}")
+    def load_model(engine: str, model_name: str):
+        try:
+            model_manager.load_model(engine, model_name)
+            return {"status": "loading"}
+        except ValueError as e:
+            return {"error": str(e)}
 
     uvicorn.run(app, host=host, port=port)
