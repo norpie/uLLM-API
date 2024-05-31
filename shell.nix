4ed4d0ae110d965b5a8c1c0b7f4442a64d2863a4
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2024-05-31 21:44:05 +0200

    build: move to requirements.txt

--- shell.nix
+++ shell.nix
@@ -9,12 +9,14 @@ in
     buildInputs = with pkgs; [
       (pkgs.python311.withPackages (python-pkgs:
         with python-pkgs; [
-          fastapi
-          uvicorn
+          pip
         ]))
     ];
 
     shellHook = ''
       export PKG_CONFIG_PATH="${pkgs.pkg-config}/lib/pkgconfig"
+      export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib/:/run/opengl-driver/lib/"
+      python -m venv .venv
+      source .venv/bin/activate
     '';
   }
