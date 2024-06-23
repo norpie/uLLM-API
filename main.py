3627d9415afef8acf017f75b1947853dd8dbb77c
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2024-06-24 00:26:10 +0200

    feat: rename web -> http_server

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import cli
 import data
 import models
-import web
+import http_server
 
 
 def main():
     args = cli.parse()
     data_dir = data.DataDir(args.data_dir)
     model_manager = models.ModelManager(data_dir)
-    web.run(args.host, args.port, model_manager)
+    http_server.run(args.host, args.port, model_manager)
 
 
 if __name__ == "__main__":
