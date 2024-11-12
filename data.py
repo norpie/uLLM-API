f3d58ec706cc5bae6ec10f5d28dd7aa88ecabc2b
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2024-11-12 22:40:31 +0100

    feat: add types for datadir

--- data.py
+++ data.py
@@ -3,17 +3,20 @@ from pathlib import Path
 
 
 class DataDir:
-    def __init__(self, data_dir):
+
+    data_dir: str
+
+    def __init__(self, data_dir: str):
         if data_dir.startswith("$"):
             data_dir = os.path.expandvars(data_dir)
         if not os.path.exists(data_dir):
             os.makedirs(data_dir)
         self.data_dir = data_dir
 
-    def get_root(self):
+    def get_root(self) -> str:
         return self.data_dir
 
-    def get_model_path(self):
+    def get_model_path(self) -> Path:
         if not os.path.exists(os.path.join(self.data_dir, "models")):
             os.makedirs(os.path.join(self.data_dir, "models"))
         return Path(os.path.join(self.data_dir, "models"))
