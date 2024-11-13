a6b2a719e5234b268fc5457c6f62076b0e4c47a0
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2024-11-13 19:52:36 +0100

    refactor: rename llama-cpp file

--- engines/llama_cpp.py
+++ engines/llama_cpp.py
@@ -8,9 +8,10 @@ from llama_cpp import Llama
 class LlamaCppEngine(Engine):
     def __init__(self, path: str):
         self.path = path
+        self.streaming = False
 
     def load_model(self) -> None:
-        self.llama = Llama(self.path, n_ctx=4096)
+        self.llama = Llama(str(self.path), n_ctx=4096, n_gpu_layers=99999)
 
     def unload_model(self) -> None:
         if self.llama:
@@ -18,7 +19,6 @@ class LlamaCppEngine(Engine):
         pass
 
     def apply_parameters(self, parameters: EngineParameters) -> None:
-        self.llama.settings()
         pass
 
     def cancel_streaming(self) -> None:
@@ -30,6 +30,7 @@ class LlamaCppEngine(Engine):
         prompt: str,
         stream: Callable[[str], Awaitable[None]] | None,
     ) -> str:
+        self.streaming = True
         completion = ""
         stop_reason = ""
         time_start = time.time()
@@ -48,16 +49,18 @@ class LlamaCppEngine(Engine):
             mirostat_eta=parameters.mirostat_eta,
             mirostat_tau=parameters.mirostat_tau,
         )
+        token_count = 0
         for token in generator:
             detokenized = self.llama.detokenize([token])
 
             chunk = detokenized.decode()
             completion += chunk
+            token_count += 1
+            print(chunk, end="", flush=True)
 
             if stream:
                 await stream(chunk)
-
-            if len(completion) >= parameters.max_tokens:
+            if token_count >= parameters.max_tokens:
                 stop_reason = "max_tokens"
                 break
             ends, seq = parameters.ends_with_stop_sequence(chunk)
@@ -71,6 +74,6 @@ class LlamaCppEngine(Engine):
             stop_reason = "EOS"
         time_end = time.time()
         print(
-            f"Generated {len(completion)} tokens in {time_end - time_start:.2f}s at a rate of {len(completion) / (time_end - time_start):.2f} tokens/s, generation stopped because of {stop_reason}"
+            f"Generated {token_count} tokens in {time_end - time_start:.2f}s at a rate of {token_count / (time_end - time_start):.2f} tokens/s, generation stopped because of {stop_reason}"
         )
         return completion
