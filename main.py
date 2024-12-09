fd4e43881f4aa22f15e66c6393fd66ec2da98091
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2024-12-09 10:20:03 +0100

    refactor: more lazyloading in main

--- main.py
+++ main.py
@@ -1,29 +1,23 @@
-import cli
-import time
-
-
 def main():
+    import cli
     args = cli.parse()
     if not args.http and not args.sockets and not args.rabbitmq:
         print(f"Please specify at least one server type to start")
         return
+
     import data
-
     data_dir = data.DataDir(args.data_dir)
-    import models
 
+    import models
     model_manager = models.ModelManager(data_dir)
     if args.http:
         import http_server
-
         http_server.start(args.host, args.http_port, model_manager)
     if args.sockets:
         import sockets_server
-
         sockets_server.start(args.host, args.sockets_port, model_manager)
     if args.rabbitmq:
         import rabbitmq
-
         rabbitmq.start(
             args.rabbitmq_host,
             args.rabbitmq_port,
@@ -34,6 +28,7 @@ def main():
             args.rabbitmq_password,
             model_manager,
         )
+    import time
     time.sleep(1)
 
 
