21940c3b211c7166fa6a08e4589a953d1f6daaba
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2024-12-09 10:19:47 +0100

    feat: added rabbitmq to main

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 
 def main():
     args = cli.parse()
-    if not args.http and not args.sockets:
+    if not args.http and not args.sockets and not args.rabbitmq:
         print(f"Please specify at least one server type to start")
         return
     import data
@@ -15,10 +15,25 @@ def main():
     model_manager = models.ModelManager(data_dir)
     if args.http:
         import http_server
+
         http_server.start(args.host, args.http_port, model_manager)
     if args.sockets:
         import sockets_server
+
         sockets_server.start(args.host, args.sockets_port, model_manager)
+    if args.rabbitmq:
+        import rabbitmq
+
+        rabbitmq.start(
+            args.rabbitmq_host,
+            args.rabbitmq_port,
+            args.rabbitmq_vhost,
+            args.rabbitmq_queue,
+            args.rabbitmq_reply_queue,
+            args.rabbitmq_username,
+            args.rabbitmq_password,
+            model_manager,
+        )
     time.sleep(1)
 
 
